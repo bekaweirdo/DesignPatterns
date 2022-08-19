8fe0404f030cf520371243cda7fe3cd2191e1238
bekachachua <beka.chachua@space.ge>
2022-08-19 18:17:13 +0400

    change creational

--- src/main/kotlin/creational/Creational.kt
+++ src/main/kotlin/creational/Creational.kt
@@ -1,8 +1,15 @@
 package creational
 
+import creational.factory_method.Country
+import creational.factory_method.FactoryMethod
 import creational.singleton.Singleton
 
 fun main(){
+    //Singleton
     Singleton.printMovies(Singleton.cartoons)
 //    print(NoMovies[0])
+    //Factory Method
+    println(FactoryMethod().currency(Country.UK).symbol())
+    println(FactoryMethod().currency(Country.Spain).code())
+    println(FactoryMethod().currency(Country.UnitedStates).symbol())
 }
